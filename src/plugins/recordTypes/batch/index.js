035c65fcb8cf469722e00c67b76c1fb676ad110f
cesarvh <cesarv.h@berkeley.edu>
2019-04-09 15:53:29 -0700

    CC-489: Added batch job template

--- src/plugins/recordTypes/batch/index.js
+++ src/plugins/recordTypes/batch/index.js
@@ -1,6 +1,8 @@
 import columns from './columns';
 import messages from './messages';
 import serviceConfig from './serviceConfig';
+import forms from './forms';
+import fields from './fields';
 
 export default () => configContext => ({
   recordTypes: {
@@ -8,6 +10,8 @@ export default () => configContext => ({
       messages,
       serviceConfig,
       columns: columns(configContext),
+      forms: forms(configContext),
+      fields: fields(configContext),
     },
   },
 });
