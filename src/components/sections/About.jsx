15e399b05b99782f94cab6cca0ff108ada19cf93
Ray Lee <ray.lee@lyrasis.org>
2019-07-08 19:21:01 -0700

    DRYD-689: Update login page links.

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -16,6 +16,45 @@ const messages = defineMessages({
         CollectionSpace is a community-supported collections management application for museums,
         historical societies, natural science collections, and more.
       </p>
+      <p>
+        This demo site is running the
+        <a href="http://www.collectionspace.org/current_release/">current version</a> of the
+        <i>common</i> installation, which includes fields and procedures common to most collecting
+        organizations. If you’re interested in a version of CollectionSpace configured to meet the
+        needs of a specific domain, please visit our
+        <a href="https://www.collectionspace.org/demo/">demo landing page</a>
+        to view all available options. Examples include Anthropology, Botanical Garden, Fine Art,
+        Herbarium, and Local History &amp; Material Culture.
+      </p>
+      <p>
+        To log in, use the email <strong>admin@core.collectionspace.org</strong>, with the
+        case-sensitive password <strong>Administrator</strong>. To view in read-only mode, use the
+        email <strong>reader@core.collectionspace.org</strong> with the password
+        <strong>reader</strong>.
+      </p>
+      <p>
+        Not sure where to get started? Follow along with one of our quick-start guides:
+      </p>
+      <ul>
+        <li>
+          <span>Create a new object:</span>
+          <a href="http://bit.ly/newobjpdf">PDF</a>
+          <a href="https://vimeo.com/112212895">Screencast</a>
+        </li>
+        <li>
+          <span>Link to an image:</span>
+          <a href="http://bit.ly/linkimgpdf">PDF</a>
+          <a href="https://vimeo.com/112214418">Screencast</a>
+        </li>
+        <li>
+          <span>Add a storage location:</span>
+          <a href="http://bit.ly/storlocpdf">PDF</a>
+          <a href="https://vimeo.com/112818120">Screencast</a>
+        </li>
+      </ul>
+      <p>
+        We rebuild this demo site weekly so don't worry, you won't break it!
+      </p>
     `,
   },
 });
