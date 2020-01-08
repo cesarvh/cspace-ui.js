d652f34793a72a27cdc391b1ae99dda4cff92a2e
cesarvh <cesarv.h@berkeley.edu>
2020-01-08 15:27:58 -0800

    CC-538/DRYD-831: Made changes to the way mimetypes are retrieved

--- src/components/invocable/InvocationModal.jsx
+++ src/components/invocable/InvocationModal.jsx
@@ -237,14 +237,14 @@ export default class InvocationModal extends Component {
       invocationDescriptor,
     } = this.state;
 
-    let mimeList = []
+    let mimeList = [];
+
     if (getMimeTypes) {
-      mimeList = getMimeTypes(csid)
-      mimeList = mimeList.getIn(['document', 'ns2:reports_common', 'supportsOutputMIMEList', 'outputMIME']);
+      mimeList = getMimeTypes(csid).getIn(['document', 'ns2:reports_common', 'supportsOutputMIMEList', 'outputMIME']);
     }
 
-    const prefilter = (option) => mimeList.contains(option.value);
-    
+    const prefilter = option => mimeList.contains(option.value);
+
     if (recordType === 'report') {
       let mimeList = [];
 
@@ -262,8 +262,8 @@ export default class InvocationModal extends Component {
           <OptionPickerInput
             blankable={false}
             label={<Label><FormattedMessage {...messages.format} /></Label>}
-            source= {"reportMimeTypes"}
-            prefilter= {mimeList ? prefilter : null}
+            source={'reportMimeTypes'}
+            prefilter={mimeList ? prefilter : null}
             value={invocationDescriptor.get('outputMIME')}
             onCommit={this.handleFormatPickerCommit}
             // blankable
@@ -273,8 +273,6 @@ export default class InvocationModal extends Component {
     }
     return null;
   }
-
-
 
   renderButtonBar() {
     const {
