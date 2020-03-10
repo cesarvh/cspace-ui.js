b15e8c6c53899f99341c8a1e61ee3b0127353600
Ray Lee <ray.lee@lyrasis.org>
2020-03-10 00:01:45 -0700

    DRYD-827: Fix incorrect term picker source.

--- src/plugins/recordTypes/uoc/fields.js
+++ src/plugins/recordTypes/uoc/fields.js
@@ -869,7 +869,7 @@ export default (configContext) => {
             view: {
               type: TermPickerInput,
               props: {
-                source: 'uocSubcollections',
+                source: 'uocsubcollections',
               },
             },
           },
